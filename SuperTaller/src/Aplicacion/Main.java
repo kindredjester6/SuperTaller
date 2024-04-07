1176b89a4f2a43ff71e945d334e6c7f63f214bec
Oscar Campos Argueda <Oscar Campos Argueda@LAPTOP-SJR955OH.mshome.net>
2024-04-07 15:07:04 -0600

    Primer commit desde mi repo local

--- SuperTaller/src/Aplicacion/Main.java
+++ SuperTaller/src/Aplicacion/Main.java
@@ -4,6 +4,7 @@
  */
 package Aplicacion;
 
+import Interfaz.MenuTaller;
 /**
  *
  * @author Oscar Campos Argueda
@@ -14,7 +15,9 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        MenuTaller vent = new MenuTaller();
+        
+        vent.setVisible(true);
     }
     
 }
